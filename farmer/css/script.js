0e06452e1b2d20761a98e184b65976eb8d3cf0df
unknown <vvkulkarni360@gmail.com>
2023-05-04 21:12:28 +0530

    did xome changes

--- farmer/css/script.js
+++ farmer/css/script.js
@@ -2,30 +2,50 @@
 $(function () {
     // Get the canvas element and create the chart
     var ctx = document.getElementById('myChart').getContext('2d');
-    var item = document.getElementsByTagName("li")
+    // function myFunction() {
+        var item = document.getElementsByTagName("li")
+    // }
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
             datasets: [
                 {
-                    label: item,
+                    label: item[0].innerHTML,
                     data: [10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32],
                     borderColor: 'red',
                     fill: false
                 },
-                // {
-                //   label: 'Tomatoes',
-                //   data: [8, 9, 11, 12, 14, 15, 17, 18, 20, 21, 23, 25],
-                //   borderColor: 'blue',
-                //   fill: false
-                // },
-                // {
-                //   label: 'Carrots',
-                //   data: [6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 18, 20],
-                //   borderColor: 'green',
-                //   fill: false
-                // }
+                {
+                  label: item[1].innerHTML,
+                  data: [8, 9, 11, 12, 14, 10, 17, 18, 20, 21, 23, 25],
+                  borderColor: 'blue',
+                  fill: false
+                },
+                {
+                  label: item[2].innerHTML,
+                  data: [6, 7, 8, 9, 10, 12, 10, 14, 15, 16, 18, 20],
+                  borderColor: 'green',
+                  fill: false
+                },
+                {
+                  label: item[3].innerHTML,
+                  data: [6, 7, 8, 9, 10, 12, 13, 14,10, 16, 18, 20],
+                  borderColor: 'black',
+                  fill: false
+                },
+                {
+                  label: item[4].innerHTML,
+                  data: [6, 7, 8, 9, 10, 12, 13, 14, 15, 10, 18, 20],
+                  borderColor: 'brown',
+                  fill: false
+                },
+                {
+                  label: item[5].innerHTML,
+                  data: [6, 7, 8, 9, 10, 12, 13, 14, 15, 16, 10, 20],
+                  borderColor: 'yellow',
+                  fill: false
+                }
             ]
         },
         options: {
